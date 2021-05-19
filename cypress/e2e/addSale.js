a02348cbaee345dce7f273642a0f66e78006e309
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-05-19 17:31:23 +1000

    Add faker (object mocking)

--- cypress/e2e/addSale.js
+++ cypress/e2e/addSale.js
@@ -1,6 +1,12 @@
+import {generateUser, generateTask} from '../support/generate';
+
 describe ('sales list', () => {
   it('can add a sale', () => {
+    const user = generateUser();
+    const task = generateTask();
     cy.visit('/');
     cy.findByPlaceholderText(/add/i).click();
+    console.log(user);
+    console.log(task);
   })
 });
