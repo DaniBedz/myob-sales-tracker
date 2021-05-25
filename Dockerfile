a6602ae9048771c0989814c069331167f9ea3b5f
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-05-26 00:51:30 +1000

    Docker fixes

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 # Use Node.js 14 Alpine
 FROM node:14-alpine
 
-# Mode package.json into a tmp directory
+# Move package.json into a tmp directory
 ADD package.json /tmp/package.json
 
 # Remove the old build directory
 RUN rm -rf build
 
 # Install the dependancies
-RUN cd /tmp && npm install -q
+RUN cd /tmp && npm install
 
 ADD ./ /src
 
--- build.js
+++ build.js
@@ -1,10 +1,8 @@
 const { build } = require('esbuild');
 const chokidar = require('chokidar');
-const fs = require('fs-extra');
 
-const generateBuild = async () => {
-  await fs.rmdirSync('./build/static', { recursive: true });
-  await build({
+const generateBuild = () => {
+  build({
     entryPoints: ['./src/index.jsx'],
     outdir: './build/static/js',
     minify: true,
@@ -13,15 +11,6 @@ const generateBuild = async () => {
     sourcemap: true,
     loader: { '.svg': 'dataurl', '.png': 'dataurl', '.woff2': 'file' },
   }).catch(() => process.exit(1));
-  await fs.move(
-    './build/static/js/index.css',
-    './build/static/css/index.css',
-    (err) => {
-      if (err) return console.error(err);
-      console.log('success!');
-      return null;
-    }
-  );
 };
 chokidar
   .watch('.', { ignored: /build|node_modules|.git/ })
