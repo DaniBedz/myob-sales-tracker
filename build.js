2abe8ab7cde49bac24143644f2d65a92415d0c71
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-05-26 01:17:26 +1000

    Removed docker files

--- build.js
+++ build.js
@@ -1,16 +1,26 @@
 const { build } = require('esbuild');
 const chokidar = require('chokidar');
+const fs = require('fs-extra');
 
-const generateBuild = () => {
-  build({
+const generateBuild = async () => {
+  await fs.rmdirSync('./build/static', { recursive: true });
+  await build({
     entryPoints: ['./src/index.jsx'],
     outdir: './build/static/js',
     minify: true,
     bundle: true,
-    platform: 'node',
     sourcemap: true,
     loader: { '.svg': 'dataurl', '.png': 'dataurl', '.woff2': 'file' },
   }).catch(() => process.exit(1));
+  await fs.move(
+    './build/static/js/index.css',
+    './build/static/css/index.css',
+    (err) => {
+      if (err) return console.error(err);
+      console.log('success!');
+      return null;
+    }
+  );
 };
 chokidar
   .watch('.', { ignored: /build|node_modules|.git/ })
