fcccf287f3a06ab57aaf77278951c9c83bc9ec34
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-05-26 01:55:29 +1000

    remove fs dependency

--- build.js
+++ build.js
@@ -1,26 +1,17 @@
 const { build } = require('esbuild');
 const chokidar = require('chokidar');
-const fs = require('fs-extra');
 
-const generateBuild = async () => {
-  await fs.rmdirSync('./build/static', { recursive: true });
-  await build({
+const generateBuild = () => {
+  build({
     entryPoints: ['./src/index.jsx'],
     outdir: './build/static/js',
     minify: true,
+    platform: 'node',
+    target: ['node14.17'],
     bundle: true,
     sourcemap: true,
     loader: { '.svg': 'dataurl', '.png': 'dataurl', '.woff2': 'file' },
   }).catch(() => process.exit(1));
-  await fs.move(
-    './build/static/js/index.css',
-    './build/static/css/index.css',
-    (err) => {
-      if (err) return console.error(err);
-      console.log('success!');
-      return null;
-    }
-  );
 };
 chokidar
   .watch('.', { ignored: /build|node_modules|.git/ })
