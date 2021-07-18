6ec516410d54d87d7895235c04c080c5b0c270b7
Dani Bednarski <49581958+DaniBedz@users.noreply.github.com>
2021-07-18 15:05:33 +1000

    Save to DB

--- src/components/Contexts/SalesListContext.jsx
+++ src/components/Contexts/SalesListContext.jsx
@@ -1,6 +1,9 @@
 import React, {
     createContext, useState, useEffect, useCallback,
 } from 'react';
+import {
+    doc, setDoc, getFirestore,
+} from 'firebase/firestore';
 import * as alertify from 'alertifyjs';
 import 'alertifyjs/build/css/alertify.css';
 import '../SaleList/alertifyCustom.css';
@@ -8,6 +11,10 @@ import '../SaleList/alertifyCustom.css';
 export const SalesListContext = createContext();
 
 export function SalesListContextProvider(props) {
+    const { uid } = JSON.parse(localStorage.getItem('user'));
+    const db = getFirestore();
+    const docRef = doc(db, 'users', uid);
+
     const [sales, setSales] = useState(
         JSON.parse(window.localStorage.getItem('sales')) || [],
     );
@@ -23,6 +30,18 @@ export function SalesListContextProvider(props) {
         }
         window.localStorage.setItem(text, JSON.stringify(object));
     }, []);
+
+    async function writeToDB() {
+        try {
+            await setDoc(docRef, {
+                sales,
+                archivedSales,
+            });
+        } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error(e);
+        }
+    }
 
     function handleChange(input, propName, sale) {
         const index = sales.indexOf(sale);
@@ -40,10 +59,12 @@ export function SalesListContextProvider(props) {
 
     useEffect(() => {
         saveToLocalStorage(sales, 'sales');
+        writeToDB();
     }, [sales, saveToLocalStorage]);
 
     useEffect(() => {
         saveToLocalStorage(archivedSales, 'archivedSales');
+        writeToDB();
     }, [archivedSales, saveToLocalStorage]);
 
     const { children } = props;
